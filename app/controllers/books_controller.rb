ebd7f564ed38d9d37ce20a4f827743f0307e2cf1
yuri-misonoo <yuri.panpanda@gmail.com>
2021-07-02 06:48:06 +0000

    create index page

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def edit
+  end
+  
+  private
+  def book_params
+    params. require(:book). permit(:title, :body)
+  end
+  
+end
