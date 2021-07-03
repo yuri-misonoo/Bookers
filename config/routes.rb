7dd07702de0637c6aaba12da771fba12cd257703
yuri-misonoo <yuri.panpanda@gmail.com>
2021-07-03 14:44:01 +0000

    Flash error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :books
-  get '/' => 'homes#top'
+  root to: 'homes#top'
   delete 'books/:id' => 'books#destroy', as:'destroy_book'
 
 end
