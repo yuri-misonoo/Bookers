f5c1e1324610498d90e67842b3a80ca69b4bf337
yuri-misonoo <yuri.panpanda@gmail.com>
2021-07-05 05:06:28 +0000

    before CSS

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,9 +11,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id), notice: 'Book was successfully creates'
+      redirect_to book_path(@book.id), notice: 'Book was successfully created'
     
     else
+      @books = Book.all
       render :index
     end
   end
